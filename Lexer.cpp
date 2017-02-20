03ecb8e34672217a47b906da3f066c9bff4551e1
SchroterQuentin <quentin.schroter@numericable.fr>
2017-02-20 16:36:27 +0100

    create class Lexer

---
+++ Lexer.cpp
@@ -0,0 +1,39 @@
+/*************************************************************************
+TPComp
+Lexer.cpp  -  Implémentation de la classe Lexer
+-------------------
+début                : 15/02/2017
+copyright            : (C)2015 par FOLLEAS Jacques et SCHROTER Quentin
+*************************************************************************/
+
+//---------- Réalisation de la classe Lexer (fichier Lexer.cpp) --
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Lexer.h"
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+
+//----- Constructeur
+Lexer::Lexer()
+{}// Bloc vide
+//----- Fin constructeur
+
+//----- Destructeur
+Lexer::~Lexer()
+{}// Bloc vide
+//----- Fin destructeur
